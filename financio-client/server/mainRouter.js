b720dc3467973ce6915c2a742e5c17a9258d898a
Miki-herman <her.mikiherman@gmail.com>
2023-12-17 01:07:52 +0100

    update

--- financio-client/server/mainRouter.js
+++ financio-client/server/mainRouter.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+// transaction/delete
+
+// transaction/create
+
+// transaction/edit
+
+// transaction/list
+
+// overview/get
+
+// overview/list
+
+// graph/get
+
+// savingGoal/edit
+
+// savingGoal/get
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
