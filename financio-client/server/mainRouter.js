bac180ac61f9f09883ae58ded5dec4cab3496c2b
Miki-herman <her.mikiherman@gmail.com>
2023-12-17 16:34:57 +0100

    update

--- financio-client/server/mainRouter.js
+++ financio-client/server/mainRouter.js
@@ -3,22 +3,49 @@ const app = express();
 const port = 3000;
 
 // transaction/delete
+app.delete("transaction/delete", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // transaction/create
+app.post("transaction/create", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // transaction/edit
+app.post("transaction/edit", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // transaction/list
+app.get("transaction/list", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // overview/get
+app.get("overview/get", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // overview/list
+app.get("overview/list", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // graph/get
+app.get("graph/get", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // savingGoal/edit
+app.post("savingGoal/edit", (req, res) => {
+    res.status(200).send("Working")
+});
 
 // savingGoal/get
+app.get("savingGoal/get", (req, res) => {
+    res.status(200).send("Working")
+});
 
 
 app.listen(port, () => {
